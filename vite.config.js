e8ad4c1f4c1f54a1f5a98006b1088adf24995080
Developer Yousuf <158748127+dev-hmyousuf@users.noreply.github.com>
2024-09-24 09:37:51 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Other options...
+  build: {
+    rollupOptions: {
+      external: ['axios'],
+    },
+  },
 });
